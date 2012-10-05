976d36e7e9bf41ee179e997c36c8b9af0055b9b4
Linge Dai <linge@rice.edu>
2012-10-05 02:43:18 -0500

    finished everything

---
+++ Flickr/RecentFlickrViewController.h
@@ -0,0 +1,13 @@
+//
+//  RecentFlickrViewController.h
+//  Flickr
+//
+//  Created by Heaven  Chen on 10/4/12.
+//  Copyright (c) 2012 Rice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecentFlickrViewController : UITableViewController
+
+@end
