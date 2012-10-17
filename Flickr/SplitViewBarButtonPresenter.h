de0ec9cffaafbdf2de43fc2fa0ff4d297f2da6e4
Linge Dai <linge@rice.edu>
2012-10-16 22:16:47 -0500

    The popover button shows up when starting

---
+++ Flickr/SplitViewBarButtonPresenter.h
@@ -0,0 +1,14 @@
+//
+//  SplitViewBarButtonPresenter.h
+//  Calculator
+//
+//  Created by Linge Dai on 10/1/12.
+//  Copyright (c) 2012 Rice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol SplitViewBarButtonPresenter <NSObject>
+@property (nonatomic, strong) UIBarButtonItem *splitViewBarButtonItem;
+
+@end
